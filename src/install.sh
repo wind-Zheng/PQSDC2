e09d20d4db8d156ba448940c8b0d49171136079d
wind-Zheng <52390056+wind-Zheng@users.noreply.github.com>
2024-09-06 20:12:10 +0800

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 1 安装配置环境依赖https://github.com/zpaq/zpaq.git
-git clone https://github.com/zpaq/zpaq.git 这里已经下载完毕
-cd zpaq
-make
-cd ..
+#git clone https://github.com/zpaq/zpaq.git 这里已经下载完毕
+#cd zpaq
+#make
+#cd ..
 
 # 2 编译PQSDC
 g++ partition_all_ESort.cpp -std=c++11 -fopenmp -O3 -o partition_all_ESort
@@ -14,7 +14,7 @@ g++ -std=c++11 -Wall -Werror -Wno-unused-variable -O3 -o pqsdc_tools pqsdc_tools
 
 # 3 配置环境(需要外部全局环境执行)
 #export PATH=$PATH:`pwd`/
-#export PQSDC_V2_PATH="`pwd`/"
+#export PQSDC2_PATH="`pwd`/"
 #source ~/.bashrc
 
 # 4 激活运行脚本
